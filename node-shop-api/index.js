9308024a393f1f4a66532b0cd392b901243f0cea
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-01-21 20:43:56 -0500

    added api-shop

---
+++ node-shop-api/index.js
@@ -0,0 +1,33 @@
+cosnt express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require ("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute= require("./routes/order");
+cosnt stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+.connnect(process.env.Mongo_URL)
+.then(() => console.log("DB Connection Sucsessfull"))
+.cathe((err) => {
+    console.log(err);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
