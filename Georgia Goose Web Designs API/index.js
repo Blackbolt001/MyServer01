efc8af164e83732b61d7d6e09da204a1d6808630
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-02-07 01:00:04 -0500

    pushing to gh

---
+++ Georgia Goose Web Designs API/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+//const authRoute = require("./routes/auth")
+//const userRoute = require("./routes/user")
+dotenv.config();
+
+// connecting to Database
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("DBConnection Successful"))
+.catch((err)=>{
+    console.log(err);
+});
+//app.get("/api/test", () => {
+    //console.log("test is successful");
+//});
+//app.use(express.json())
+//app.use("/api/auth", authRoute)
+//app.use("/api/users", userRoute)
+
+app.listen(process.env.PORT ||5000, () =>{
+    console.log("Backend Server Baby!!!"); 
+});
